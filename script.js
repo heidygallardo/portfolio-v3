55880e4741de9c19c9bbb7a3e3368c98fd98ad6b
Heidy Gallardo <heidy.gallardo01@utrgv.edu>
2025-01-31 16:08:59 -0600

    added automatic close of menu after option selected

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // mobile view menu bar icon
 const menuBarIcon = document.querySelector('#menu-bar-icon');
 const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links a'); 
 
 // toggle nav links menu on click 
 menuBarIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+// close menu after user selects menu item
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active'); // Close the menu
+    });
+});
